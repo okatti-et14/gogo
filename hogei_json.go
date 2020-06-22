f96ee6ebd86b7174a3a88a475fd8f5c62c82daa8
takumi.iioka <iioka.takumi@ebisol.co.jp>
2020-06-22 15:14:56 +0900

    時間をjsonで扱う

--- hogei_json.go
+++ hogei_json.go
@@ -4,16 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 )
 
 // User is
 type User struct {
-	ID   int
-	Name *string
+	ID       int        `json:"idid"`
+	Name     *string    `json:"namename,omitempty"`
+	BirthDay *time.Time `json:"birthday"`
 }
 
 func main() {
-	src := `{"Id":3,"Name":"hogei"}`
+	hogeiUnmarshal()
+	hogeiMarshal()
+}
+
+func hogeiUnmarshal() {
+	src := `{"idid":3,"namename":"hogei","birthday":"2015-09-18T00:00:00+09:00"}`
 	u := &User{}
 	err := json.Unmarshal([]byte(src), u)
 	if err != nil {
@@ -21,4 +28,19 @@ func main() {
 	}
 	fmt.Println(u)
 	fmt.Println(u.ID)
+}
+
+func hogeiMarshal() {
+	loc, _ := time.LoadLocation("Asia/Tokyo")
+	t := time.Date(2015, 9, 18, 0, 0, 0, 0, loc)
+	u := User{
+		BirthDay: &t,
+		ID:       1,
+	}
+
+	j, err := json.Marshal(u)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(j))
 }
