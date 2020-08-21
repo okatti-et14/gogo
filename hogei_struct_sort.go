9618cc2037eeffdb39068f55d6ef242754b64250
takumi.iioka <takumi.iioka@ebisol.co.jp>
2020-08-21 18:04:51 +0900

    pointerでも確認

--- hogei_struct_sort.go
+++ hogei_struct_sort.go
@@ -6,25 +6,35 @@ import (
 )
 
 type Tmp struct {
-	ID   int
-	Name string
-	Age  int
+	ID   *int
+	Name *string
+	Age  *int
 }
 
 func main() {
-	tmp := []Tmp{
-		{ID: 12, Name: "aac", Age: 110},
-		{ID: 12, Name: "aac", Age: 100},
-		{ID: 12, Name: "aab", Age: 100},
-		{ID: 11, Name: "aaa", Age: 100}}
+	i12 := 12
+	i11 := 11
+	saac := "aac"
+	saab := "aab"
+	saaa := "aaa"
+	i110 := 110
+	i100 := 100
+	tmp := []*Tmp{
+		{ID: &i12, Name: &saaa, Age: &i110},
+		{ID: &i12, Name: &saac, Age: &i100},
+		{ID: &i12, Name: &saab, Age: &i100},
+		{ID: &i11, Name: &saaa, Age: &i100}}
 	sort.SliceStable(tmp, func(i, j int) bool {
-		return tmp[i].Age < tmp[j].Age
+		return *tmp[i].Age < *tmp[j].Age
 	})
 	sort.SliceStable(tmp, func(i, j int) bool {
-		return tmp[i].Name < tmp[j].Name
+		return *tmp[i].Name < *tmp[j].Name
 	})
 	sort.SliceStable(tmp, func(i, j int) bool {
-		return tmp[i].ID < tmp[j].ID
+		return *tmp[i].ID < *tmp[j].ID
 	})
+	for _, b := range tmp {
+		fmt.Println("id:", *b.ID, ",name:", *b.Name, ",age:", *b.Age)
+	}
 	fmt.Print(tmp)
 }
